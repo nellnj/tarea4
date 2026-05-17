7ebf8d160d6d6757643562e1fc99a00dab704127
nellnj <nellyjhonnegretejimenez@gmail.com>
2026-05-17 16:26:09 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,43 @@
+from clientes.clientes import Cliente
+from servicios.servicios import *
+from reserva.reserva import Reserva
+from excepciones.excepciones import *
+import logging
+
+clientes = []
+servicios = []
+reservas = []
+
+print("\n SOFTWARE FJ\n")
+
+
+operaciones = [
+
+    lambda: clientes.append(Cliente("Carlos", "12345", "carlos@gmail.com")),
+
+    lambda: clientes.append(Cliente("", "22", "error")),  # error
+
+    lambda: servicios.append(ReservaSala("Sala", 100)),
+
+    lambda: servicios.append(AlquilerEquipo("Laptop", 80)),
+
+    lambda: servicios.append(AsesoriaEspecializada("IA", 150)),
+
+    lambda: reservas.append(Reserva(clientes[0], servicios[0], 2)),
+
+    lambda: reservas[0].confirmar(),
+]
+
+
+for i, op in enumerate(operaciones, 1):
+
+    print(f"Operación {i}")
+
+    try:
+        op()
+
+    except Exception as e:
+        print("Error:", e)
+        logging.error(str(e))
+
+print("\nSistema finalizado")
